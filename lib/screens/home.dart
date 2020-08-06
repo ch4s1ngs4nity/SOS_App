f31d51ee2635a64a83d169c88fc10cb384bcf602
Delogrand770 <delogrand@icloud.com>
2020-08-05 21:31:57 -0400

    - Updated the home screen to have a logo and welcome message

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:sos_app/widgets/drawer.dart';
 import 'package:sos_app/constants.dart' as constant;
+import 'package:sos_app/widgets/logo.dart';
 
 class HomeScreen extends StatelessWidget {
-  Widget buildScreen() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
-      child: new Text("Welcome to the SOS Feedback App"),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Home')),
       drawer: AppDrawer(constant.Route.home),
-      body: Stack(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          buildScreen()
+          Container(
+            child: logoWidget(),
+            alignment: Alignment.center,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+          child: new Text("Welcome to the SOS Feedback App"),
+            alignment: Alignment.center,
+          )
         ],
       ),
     );
